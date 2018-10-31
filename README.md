39d65dcfe3a3a2676458c762bef9e0f62b0e7372
navidshayanfar <navidshayanfar@yahoo.com>
2018-10-31 11:15:55 +0100

    edited

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Guess the number of files in the current directory
 The number of lines of code contained in guessinggame.sh is:
 34
 
-Mo 29. Okt 07:48:27 CET 2018
+Mi 31. Okt 11:15:17 CET 2018
--- guessinggame.sh
+++ guessinggame.sh
@@ -2,33 +2,33 @@
 # file name: project
 
 
+# This function is a search engine and guide the user to a correct guess. It compare the guess with the exact value of the file numbers and if the guess is higher or lower, it will provide corresponding information and ask for another guess.
 
 function search_engine {
         if [[ $guess -gt $num_files ]]
         then
                 echo "Your guess is too high. Please provide a lower value and press enter again:"
                 read guess
-        elif [[ $guess -eq $num_files ]]
+        elif [[ $guess -lt $num_files ]]
         then
-                echo "Congratulations! The correct number of files in the current directory is: $guess"
-                break
-        else
                 echo "Your guess is too low. Please provide a higher value and press enter again:"
                 read guess
         fi
 }
 
 
+# Here we find the total number of files and folders in the current directory and substract the number of folders from total to compute the number of files.
 
 num_total=$(ls $PWD | wc -l)
 num_folders=$(ls -p $PWD | grep / | wc -l)
 num_files=$(echo "$num_total - $num_folders" | bc -l)
-#echo $num_total $num_folders $num_files
+
+#Main body of program
+
 echo "How many files are in the current directory? Guess something! Write a number that you think is true and press enter:"
-count=1
 read guess
-while [[ $count -gt 0 ]]
+while [[ $num_files -ne $guess ]]
 do
 	search_engine $guess
-	count=$(echo "$count+1" | bc -l)
 done
+echo "Congratulations! The correct number of files in the current directory is: $guess"
